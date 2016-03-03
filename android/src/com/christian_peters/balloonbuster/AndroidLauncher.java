207e1158bc92f1256e877f4e965f55c6cdc786d8
cxan96 <c.p.xanten@gmail.com>
2016-03-03 18:29:53 +0100

    Initial commit

---
+++ android/src/com/christian_peters/balloonbuster/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.christian_peters.balloonbuster;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.christian_peters.balloonbuster.BalloonBusterGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BalloonBusterGame(), config);
+	}
+}
---
+++ desktop/src/com/christian_peters/balloonbuster/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.christian_peters.balloonbuster.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.christian_peters.balloonbuster.BalloonBusterGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BalloonBusterGame(), config);
+	}
+}
