acab5f3ecd71aea38cf078611e4a6eb2ad357c0b
cxan96 <c.p.xanten@gmail.com>
2016-03-07 15:18:11 +0100

    - started HUD

--- core/src/com/christian_peters/balloonbuster/scenes/PlayHUD.java
+++ core/src/com/christian_peters/balloonbuster/scenes/PlayHUD.java
@@ -3,7 +3,9 @@ package com.christian_peters.balloonbuster.scenes;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import com.christian_peters.balloonbuster.BalloonBusterGame;
 import com.christian_peters.balloonbuster.screens.PlayScreen;
 
 /**
@@ -12,14 +14,16 @@ import com.christian_peters.balloonbuster.screens.PlayScreen;
 public class PlayHUD {
     private Stage stage;
     private Viewport viewport;
-    private OrthographicCamera camera;
-    private int score;
+    private float score;
     private PlayScreen screen;
     private boolean paused;
 
     public PlayHUD(SpriteBatch batch, PlayScreen screen){
         this.screen = screen;
-        //TODO pass batch to stage
+        this.score = 0;
+        this.paused = false;
+        this.viewport = new FitViewport(BalloonBusterGame.V_WIDTH, BalloonBusterGame.V_HEIGHT, new OrthographicCamera());
+        this.stage = new Stage(viewport, batch);
     }
 
     public void update(float dt){
